aa125a6270fdac2e16f54c5db8ce3dcf0cf75f99
opeyemi-jonah <gavrieljonah@gmail.com>
2020-10-23 16:05:38 -0600

    created schema for post model

--- models/post.js
+++ models/post.js
@@ -1,3 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    images: [String],
+    location: String,
+    lat: Number,
+    lng: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+module.exports = mongoose.model('Post', PostSchema);
 /*
 Post
 -title - string
@@ -10,3 +34,4 @@ Post
 -author - object id
 -reviews - array of objects
 */
+
